c6bfb1fce58c9ab53e4a251909b585977cc75bc9
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2017-02-13 11:28:48 +0100

    Add obejcts rather then classes

--- djangocms_spa/renderer_pool.py
+++ djangocms_spa/renderer_pool.py
@@ -5,18 +5,21 @@ from .renderer import BaseSPARenderer, MixinPluginRenderer
 class RendererPool(object):
     def __init__(self):
         self.renderers = {}
-
-    def register_renderer(self, renderer: BaseSPARenderer):
+    
+    def register_renderer(self, renderer: BaseSPARenderer.__class__):
+        self._register_renderer(renderer())
+    
+    def _register_renderer(self, renderer: BaseSPARenderer):
         self.renderers[renderer.plugin_class.__name__] = renderer
-
+    
     def register_plugin(self, plugin_class: 'SPAPluginMixin'):
         if not issubclass(plugin_class, SPAPluginMixin):
             raise TypeError()
-
+        
         renderer = MixinPluginRenderer(plugin_class)
-        self.register_renderer(renderer)
+        self._register_renderer(renderer)
         return renderer
-
+    
     def renderer_for_plugin(self, plugin) -> BaseSPARenderer:
         plugin_class = plugin.__class__
         renderer = self.renderers.get(plugin_class.__name__, None)
@@ -25,7 +28,8 @@ class RendererPool(object):
                 renderer = self.register_plugin(plugin_class)
             except TypeError:
                 pass
-
+        
         return renderer
+
 
 renderer_pool = RendererPool()
