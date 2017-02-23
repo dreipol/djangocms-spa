b102ac249d5e4dd4a5019cae69935efd22708330
Christian Schürmann <christian.schuermann@bluewin.ch>
2017-02-23 15:24:43 +0100

    Handle 404

--- djangocms_spa/views.py
+++ djangocms_spa/views.py
@@ -124,10 +124,10 @@ class SpaCmsPageDetailApiView(SpaApiView):
     cms_page_title = None
 
     def get(self, request, **kwargs):
-        self.cms_page = get_page_from_request(request, use_path=kwargs.get('path', ''))
-        self.cms_page_title = self.cms_page.title_set.get(language=request.LANGUAGE_CODE)
-
-        if not self.cms_page or not self.cms_page_title:
+        try:
+            self.cms_page = get_page_from_request(request, use_path=kwargs.get('path', ''))
+            self.cms_page_title = self.cms_page.title_set.get(language=request.LANGUAGE_CODE)
+        except AttributeError:
             return JsonResponse(data={}, status=404)
 
         return super(SpaCmsPageDetailApiView, self).get(request, **kwargs)
