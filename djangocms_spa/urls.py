4a4dbfd142e2f8fca3e82d7790ace4ed88bb0b3f
Christian Schürmann <christian.schuermann@bluewin.ch>
2017-06-21 15:09:42 +0200

    Remove language code from path
    We no longer need the language detection in the URL. The locale
    middleware already handles the language properly and we can consume
    it from the request.

--- djangocms_spa/urls.py
+++ djangocms_spa/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import SpaCmsPageDetailApiView
 
 urlpatterns = [
-    url(r'^(?P<language_code>[\w-]+)/pages/$', SpaCmsPageDetailApiView.as_view(), name='cms_page_detail_home'),
-    url(r'^(?P<language_code>[\w-]+)/pages/(?P<path>.*)/$', SpaCmsPageDetailApiView.as_view(), name='cms_page_detail'),
+    url(r'^pages/$', SpaCmsPageDetailApiView.as_view(), name='cms_page_detail_home'),
+    url(r'^pages/(?P<path>.*)/$', SpaCmsPageDetailApiView.as_view(), name='cms_page_detail'),
 ]
