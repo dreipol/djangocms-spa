86d18681f08ff4d9367cce13055c817308d2581f
Christian Schürmann <christian.schuermann@bluewin.ch>
2018-06-27 13:04:53 +0200

    Make initial optional

--- djangocms_spa/apps.py
+++ djangocms_spa/apps.py
@@ -8,13 +8,13 @@ class DjangoCmsSpaConfig(AppConfig):
         from django.forms import CheckboxInput, RadioSelect, Select, SelectMultiple
         from .form_helpers import get_placeholder_for_choices_field, get_serialized_choices_for_field
 
-        CheckboxInput.render_spa = lambda self, field, initial: {
+        CheckboxInput.render_spa = lambda self, field, initial=None: {
             'items': get_serialized_choices_for_field(field=field),
             'type': 'checkbox',
             'multiline': True,
         }
 
-        RadioSelect.render_spa = lambda self, field, initial: {
+        RadioSelect.render_spa = lambda self, field, initial=None: {
             'items': get_serialized_choices_for_field(field=field),
             'type': 'radio',
             'multiline': True,
